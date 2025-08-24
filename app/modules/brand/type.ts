75c21897af236f570d18786adb37920646c4aa36
Mochamad Irvan <mchmdirvaan@gmail.com>
2025-08-25 04:40:12 +0700

    feat: fetch data brands from api

---
+++ app/modules/brand/type.ts
@@ -0,0 +1,8 @@
+export type BrandType = {
+  id: string;
+  slug: string;
+  name: string;
+  imageUrl: string;
+  createdAt: string;
+  updatedAt: string;
+};
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,10 +2,7 @@ import type { Route } from "./+types/home";
 import { Link } from "react-router";
 
 import { Card, CardHeader, CardTitle } from "~/components/ui/card";
-
-import Samsung from "/Galaxy-S25-Ultra-Body.png?url";
-import Xiaomi from "/Xiaomi-15-Ultra-Body.png?url";
-import Iphone from "/iPhone-16-Pro-Body.png?url";
+import type { BrandType } from "~/modules/brand/type";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -14,38 +11,32 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
-export default function Home() {
-  const cards = [
-    {
-      name: "iPhone",
-      image: Iphone,
-    },
-    {
-      name: "Samsung",
-      image: Samsung,
-    },
-    {
-      name: "Xiaomi",
-      image: Xiaomi,
-    },
-  ];
+export async function clientLoader() {
+  const respon = await fetch(`${import.meta.env.VITE_BACKEND_API_URL}/brands`);
+  const brands: BrandType[] = await respon.json();
+
+  return brands;
+}
+
+export default function Home({ loaderData }: Route.ComponentProps) {
+  const brands = loaderData;
 
   return (
     <div className="flex justify-between px-40 py-10">
       <h2 className="text-5xl font-bold text-white">Shop</h2>
 
       <div className="flex min-w-2xl flex-col gap-8">
-        {cards.map((card) => (
-          <Link to="/products/iphone">
+        {brands.map((brand) => (
+          <Link key={brand.id} to={`/products/${brand.slug}`}>
             <Card className="w-full max-w-2xl cursor-pointer overflow-hidden border border-zinc-800 bg-linear-to-b from-neutral-900 to-black transition-colors duration-200 hover:border-white">
               <CardHeader className="flex justify-between">
                 <div className="flex h-80 flex-col justify-between">
                   <CardTitle className="text-3xl text-white">
-                    {card.name}
+                    {brand.name}
                   </CardTitle>
                   <p className="text-white">Premium Skins</p>
                 </div>
-                <img src={card.image} className="w-[300px]" />
+                <img src={brand.imageUrl} className="w-[300px]" />
               </CardHeader>
             </Card>
           </Link>
