b6208eaf1eaf28d0931e52a9fc5ece3c924c7036
Mochamad Irvan <mchmdirvaan@gmail.com>
2025-08-25 05:03:52 +0700

    chore: tidy up

--- app/routes.ts
+++ app/routes.ts
@@ -4,7 +4,6 @@ export default [
   layout("./layouts/app-layout.tsx", [
     route("/", "routes/home.tsx"),
     route("brand/:slug", "routes/model.tsx"),
-    route("products/iphone", "routes/iphone.tsx"),
   ]),
 
   layout("./layouts/auth-layout.tsx", [
