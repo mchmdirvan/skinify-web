543e032a8c889cd01b605119f971de7727eda14e
Mochamad Irvan <mchmdirvaan@gmail.com>
2025-08-27 20:59:51 +0700

    feat: implement fetch login

--- app/routes.ts
+++ app/routes.ts
@@ -3,6 +3,7 @@ import { type RouteConfig, layout, route } from "@react-router/dev/routes";
 export default [
   layout("./layouts/app-layout.tsx", [
     route("/", "routes/home.tsx"),
+    route("/dashboard", "routes/dashboard.tsx"),
     route("/:brandSlug", "routes/brand-slug.tsx"),
     route("/:brandSlug/:modelSlug", "routes/model-slug.tsx"),
     route("/:brandSlug/:modelSlug/:productSlug", "routes/product-slug.tsx"),
--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,12 +1,50 @@
-import { Link } from "react-router";
+import { Form, Link, redirect } from "react-router";
+import type { Route } from "./+types/login";
+
 import { Button } from "~/components/ui/button";
 import { Input } from "~/components/ui/input";
 import { Label } from "~/components/ui/label";
 import { cn } from "~/lib/utils";
 
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "Skinify - Login" },
+    { name: "description", content: "Premium mobile phone skin" },
+  ];
+}
+
+export async function clientAction({ request }: Route.ClientActionArgs) {
+  let formData = await request.formData();
+
+  let email = String(formData.get("email"));
+  let password = String(formData.get("password"));
+
+  const loginUserData = {
+    email,
+    password,
+  };
+
+  const response = await fetch(
+    `${import.meta.env.VITE_BACKEND_API_URL}/auth/login`,
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(loginUserData),
+    },
+  );
+
+  const result = await response.json();
+
+  if (!result) {
+    return null;
+  }
+
+  return redirect("/dashboard");
+}
+
 export default function Login() {
   return (
-    <form className={cn("flex flex-col gap-6")}>
+    <Form method="post" className={cn("flex flex-col gap-6")}>
       <div className="flex flex-col items-center gap-2 text-center">
         <h1 className="text-2xl font-bold text-white">Login to your account</h1>
         <p className="text-sm text-balance text-white">
@@ -18,7 +56,14 @@ export default function Login() {
           <Label htmlFor="email" className="text-white">
             Email
           </Label>
-          <Input id="email" type="email" placeholder="m@example.com" required />
+          <Input
+            className="text-white"
+            id="email"
+            type="email"
+            name="email"
+            placeholder="m@example.com"
+            required
+          />
         </div>
         <div className="grid gap-3">
           <div className="flex items-center">
@@ -26,18 +71,24 @@ export default function Login() {
               Password
             </Label>
           </div>
-          <Input id="password" type="password" required />
+          <Input
+            className="text-white"
+            id="password"
+            type="password"
+            name="password"
+            required
+          />
         </div>
         <Button type="submit" className="w-full">
           Login
         </Button>
       </div>
-      <Link to="/register" className="text-center text-sm text-white">
+      <div className="text-center text-sm text-white">
         Don&apos;t have an account?{" "}
-        <a href="#" className="underline underline-offset-4">
+        <Link to="/register" className="underline underline-offset-4">
           Sign up
-        </a>
-      </Link>
-    </form>
+        </Link>
+      </div>
+    </Form>
   );
 }
