1ecfe6cc565ace8bd52cb9e8b01b3229c1b63702
Mochamad Irvan <mchmdirvaan@gmail.com>
2025-08-30 16:36:42 +0700

    style (shop) : refactor layout

--- app/routes/shop.tsx
+++ app/routes/shop.tsx
@@ -24,26 +24,30 @@ export default function Shop({ loaderData }: Route.ComponentProps) {
   const brands = loaderData;
 
   return (
-    <div className="flex justify-between px-40 py-10">
-      <h2 className="text-5xl font-bold text-white">Shop</h2>
+    <div className="mx-10 my-10">
+      <section className="relative mt-40 flex justify-between gap-40">
+        <h2 className="font-audiowide sticky top-40 self-start text-7xl font-bold text-white">
+          Shop
+        </h2>
 
-      <div className="flex min-w-2xl flex-col gap-8">
-        {brands.map((brand) => (
-          <Link key={brand.id} to={`/${brand.slug}`}>
-            <Card className="w-full max-w-2xl cursor-pointer overflow-hidden border border-zinc-800 bg-linear-to-b from-neutral-900 to-black transition-colors duration-200 hover:border-white">
-              <CardHeader className="flex justify-between">
-                <div className="flex h-80 flex-col justify-between">
-                  <CardTitle className="text-3xl text-white">
-                    {brand.name}
-                  </CardTitle>
-                  <p className="text-white">Premium Skins</p>
-                </div>
-                <img src={brand.imageUrl} className="w-[300px]" />
-              </CardHeader>
-            </Card>
-          </Link>
-        ))}
-      </div>
+        <div className="flex flex-col gap-8">
+          {brands.map((brand) => (
+            <Link key={brand.id} to={`/${brand.slug}`}>
+              <Card className="w-full min-w-[55vw] cursor-pointer overflow-hidden border border-zinc-800 bg-linear-to-b from-neutral-900 to-black transition-colors duration-200 hover:border-white">
+                <CardHeader className="flex justify-between">
+                  <div className="flex h-96 flex-col justify-between">
+                    <CardTitle className="font-audiowide text-5xl text-white">
+                      {brand.name}
+                    </CardTitle>
+                    <p className="text-white">Premium Skins</p>
+                  </div>
+                  <img src={brand.imageUrl} className="w-[400px]" />
+                </CardHeader>
+              </Card>
+            </Link>
+          ))}
+        </div>
+      </section>
     </div>
   );
 }
