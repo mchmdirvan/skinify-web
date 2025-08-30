d457d05eb1244bddc7e9200615e15f9a2713c944
Mochamad Irvan <mchmdirvaan@gmail.com>
2025-08-30 22:13:33 +0700

    feat (model-slug) : add section review

--- app/components/ui/tabs.tsx
+++ app/components/ui/tabs.tsx
@@ -1,7 +1,7 @@
-import * as React from "react"
-import * as TabsPrimitive from "@radix-ui/react-tabs"
+import * as React from "react";
+import * as TabsPrimitive from "@radix-ui/react-tabs";
 
-import { cn } from "~/lib/utils"
+import { cn } from "~/lib/utils";
 
 function Tabs({
   className,
@@ -10,10 +10,13 @@ function Tabs({
   return (
     <TabsPrimitive.Root
       data-slot="tabs"
-      className={cn("flex flex-col gap-2", className)}
+      className={cn(
+        "flex flex-col items-center justify-center gap-2",
+        className,
+      )}
       {...props}
     />
-  )
+  );
 }
 
 function TabsList({
@@ -24,12 +27,12 @@ function TabsList({
     <TabsPrimitive.List
       data-slot="tabs-list"
       className={cn(
-        "bg-muted text-muted-foreground inline-flex h-9 w-fit items-center justify-center rounded-lg p-[3px]",
-        className
+        "text-muted-foreground inline-flex h-9 w-fit items-center justify-center gap-4 rounded-lg bg-transparent p-[1px]",
+        className,
       )}
       {...props}
     />
-  )
+  );
 }
 
 function TabsTrigger({
@@ -40,12 +43,12 @@ function TabsTrigger({
     <TabsPrimitive.Trigger
       data-slot="tabs-trigger"
       className={cn(
-        "data-[state=active]:bg-background dark:data-[state=active]:text-foreground focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:outline-ring dark:data-[state=active]:border-input dark:data-[state=active]:bg-input/30 text-foreground dark:text-muted-foreground inline-flex h-[calc(100%-1px)] flex-1 items-center justify-center gap-1.5 rounded-md border border-transparent px-2 py-1 text-sm font-medium whitespace-nowrap transition-[color,box-shadow] focus-visible:ring-[3px] focus-visible:outline-1 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:shadow-sm [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
-        className
+        "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:outline-ring dark:data-[state=active]:border-input dark:data-[state=active]:bg-input/30 inline-flex h-[calc(100%-1px)] flex-1 items-center justify-center gap-1.5 rounded-md border border-transparent px-2 py-1 text-sm font-medium whitespace-nowrap text-neutral-700 transition-[color,box-shadow] focus-visible:ring-[3px] focus-visible:outline-1 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-neutral-900 data-[state=active]:text-white data-[state=active]:shadow-sm dark:text-white dark:data-[state=active]:text-white [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
+        className,
       )}
       {...props}
     />
-  )
+  );
 }
 
 function TabsContent({
@@ -58,7 +61,7 @@ function TabsContent({
       className={cn("flex-1 outline-none", className)}
       {...props}
     />
-  )
+  );
 }
 
-export { Tabs, TabsList, TabsTrigger, TabsContent }
+export { Tabs, TabsList, TabsTrigger, TabsContent };
--- app/routes/model-slug.tsx
+++ app/routes/model-slug.tsx
@@ -1,3 +1,4 @@
+import { ShieldCheckIcon } from "lucide-react";
 import type { Route } from "./+types/model-slug";
 import { Link } from "react-router";
 
@@ -8,6 +9,8 @@ import {
   CardHeader,
   CardTitle,
 } from "~/components/ui/card";
+import { Progress } from "~/components/ui/progress";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "~/components/ui/tabs";
 import type { ModelType } from "~/modules/model/type";
 
 export function meta({}: Route.MetaArgs) {
@@ -29,7 +32,34 @@ export async function clientLoader({ params }: Route.ClientLoaderArgs) {
 export default function ModelSlugRoute({ loaderData }: Route.ComponentProps) {
   const { params, products } = loaderData;
 
-  console.log(loaderData);
+  const review = [
+    { stars: 5, percentage: 95 },
+    { stars: 4, percentage: 5 },
+    { stars: 3, percentage: 0 },
+    { stars: 2, percentage: 0 },
+    { stars: 1, percentage: 0 },
+  ];
+
+  const reviewsData = [
+    {
+      author: "Yazid Hidayatullah",
+      date: "June 18, 2025",
+      content:
+        "Pemasangannya mudah sekali, kualitas bahannya bagus sekali, warnanya cerah",
+    },
+    {
+      author: "Thommy",
+      date: "June 18, 2025",
+      content:
+        "Sejak kenal Skinify 4 atau 5 tahun lalu, setiap ganti handphone selalu langsung diamankan dengan exacoat... Dragon skin selalu jadi favorit utk semua gadget saya... Gagah dan berkarakter...",
+    },
+    {
+      author: "Fahmi Mubarok",
+      date: "June 18, 2025",
+      content:
+        "untuk webstore oke,tapi untuk pengiriman agak lama,sicepat tapi gak cepat😂",
+    },
+  ];
 
   return (
     <div className="mx-10 my-10">
@@ -51,7 +81,7 @@ export default function ModelSlugRoute({ loaderData }: Route.ComponentProps) {
             </CardDescription>
           </CardHeader>
           <CardContent>
-            <div className="grid grid-cols-2 gap-8">
+            <div className="my-10 grid grid-cols-2 gap-8">
               {products.products.map((product) => (
                 <Link
                   key={product.id}
@@ -73,6 +103,111 @@ export default function ModelSlugRoute({ loaderData }: Route.ComponentProps) {
             </div>
           </CardContent>
         </Card>
+      </section>
+
+      <section className="my-20 flex justify-center">
+        <Tabs defaultValue="reviews">
+          <TabsList>
+            <TabsTrigger value="reviews">Reviews</TabsTrigger>
+            <TabsTrigger value="installation-warranty">
+              Installation Warranty
+            </TabsTrigger>
+            <TabsTrigger value="password">FAQs</TabsTrigger>
+          </TabsList>
+          <TabsContent value="reviews" className="mt-5 space-y-10">
+            <section>
+              <p className="text-center text-xs text-amber-400">
+                How we ensure the authenticity of our reviews
+              </p>
+              <p className="text-center text-xs text-white">
+                These reviews are verified purchase reviews and cannot be
+                altered or edited.
+              </p>
+            </section>
+
+            <Card className="border border-zinc-800 bg-linear-to-b from-neutral-900 to-black px-14">
+              <CardContent className="flex gap-10">
+                <div className="flex flex-col items-center justify-center">
+                  <p className="text-8xl font-semibold text-amber-400">4.9</p>
+                  <p>⭐⭐⭐⭐⭐</p>
+                </div>
+                <div className="min-h-10 self-center border-r border-white p-0">
+                  |
+                </div>
+                <div>
+                  {review.map((rating) => (
+                    <div key={rating.stars} className="flex items-center gap-4">
+                      <p className="min-w-[3rem] text-nowrap text-white">
+                        {rating.stars} Star
+                      </p>
+                      <Progress value={rating.percentage} />
+                      <p className="text-white">{rating.percentage}%</p>
+                    </div>
+                  ))}
+                </div>
+              </CardContent>
+            </Card>
+
+            <section>
+              <p className="my-5 text-xs text-white">1-3 of 1312 Reviews</p>
+              <div className="space-y-4">
+                {reviewsData.map((review) => (
+                  <Card className="max-w-2xl border border-zinc-800 bg-linear-to-b from-neutral-900 to-black">
+                    <CardContent>
+                      <div className="flex justify-between">
+                        <div>
+                          <p className="font-semibold text-white">
+                            {review.author}
+                          </p>
+                          <p>⭐⭐⭐⭐⭐</p>
+                        </div>
+                        <p className="text-white">{review.date}</p>
+                      </div>
+                      <div className="pt-5 text-xs text-white">
+                        {review.content}
+                      </div>
+                    </CardContent>
+                  </Card>
+                ))}
+              </div>
+            </section>
+          </TabsContent>
+          <TabsContent value="installation-warranty">
+            <Card className="max-w-2xl border border-zinc-800 bg-linear-to-b from-neutral-900 to-black px-10 py-14">
+              <CardHeader>
+                <div className="flex justify-center">
+                  <ShieldCheckIcon className="text-amber-400" size={60} />
+                </div>
+              </CardHeader>
+              <CardContent className="space-y-5">
+                <p className="text-center text-sm text-white">
+                  We are very confident about how easy the skin installation is
+                  for iPhone 16. Don’t trust our word, see the reviews on the
+                  previous tab 👈🏻
+                </p>
+                <p className="text-center text-sm text-white">
+                  Unlike screen protectors or other skins that can only be
+                  applied once, our skin can be removed and applied multiple
+                  times if it doesn’t fit.
+                </p>
+                <p className="text-center text-sm text-white">
+                  If your skin is damaged on installation within 2 days after
+                  the items arrived, we will send you a new one.
+                </p>
+              </CardContent>
+            </Card>
+          </TabsContent>
+        </Tabs>
+      </section>
+
+      <section className="mx-60">
+        <p className="font-chakra-petch text-center text-xs text-white">
+          You might think that a iPhone 16 case provides enough protection.
+          However, the real threat to your iPhone 16 comes from dust, tiny sand
+          particles, or other small debris that get trapped inside the case and
+          end up scratching your device in silence. Exacoat’s iPhone 16 skin
+          adds an extra layer of protection, and it is case-friendly.
+        </p>
       </section>
 
       <section className="my-40">
