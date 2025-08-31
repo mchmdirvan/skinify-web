9791c1401dddda460d5891c70c86496860f4a04f
Mochamad Irvan <mchmdirvaan@gmail.com>
2025-08-31 12:15:45 +0700

    feat (cart): creates page cart

---
+++ app/routes/cart.tsx
@@ -0,0 +1,61 @@
+import { Card, CardContent, CardHeader } from "~/components/ui/card";
+
+import Iphone16 from "/iPhone-16.png";
+import { Button } from "~/components/ui/button";
+
+export default function Cart() {
+  return (
+    <div className="mx-52 my-20">
+      <section>
+        <Card className="border border-zinc-800 bg-linear-to-b from-neutral-900 to-black">
+          <CardHeader className="grid grid-cols-6">
+            <div></div>
+            <p className="text-white">Product</p>
+            <p className="text-white">Price</p>
+            <p className="text-white">Quantity</p>
+            <p className="text-white">Subtotal</p>
+            <div></div>
+          </CardHeader>
+
+          <CardContent>
+            <div className="mx-10 my-5 border border-t border-neutral-800"></div>
+            <div className="grid grid-cols-6 items-center">
+              <div>
+                <img src={Iphone16} />
+              </div>
+              <p className="font-semibold text-white">iPhone 16 Skins</p>
+              <p className="text-xs text-white">Rp. 129.000</p>
+              <p className="text-xs text-white">2</p>
+              <p className="text-xs text-white">Rp. 258.000</p>
+              <div></div>
+            </div>
+
+            <div className="mx-10 my-5 border border-t border-neutral-800"></div>
+            <div className="grid grid-cols-6 items-center">
+              <div>
+                <img src={Iphone16} />
+              </div>
+              <p className="font-semibold text-white">iPhone 16 Skins</p>
+              <p className="text-xs text-white">Rp. 129.000</p>
+              <p className="text-xs text-white">2</p>
+              <p className="text-xs text-white">Rp. 258.000</p>
+              <div></div>
+            </div>
+          </CardContent>
+        </Card>
+      </section>
+
+      <section>
+        <p className="font-chakra-petch my-10 text-center text-white">
+          Have a coupon/gift code? click here
+        </p>
+      </section>
+
+      <section>
+        <Button className="font-chakra-petch min-w-full cursor-pointer bg-yellow-500 transition-colors duration-300 hover:bg-yellow-400">
+          PROCEED TO CHECKOUT
+        </Button>
+      </section>
+    </div>
+  );
+}
