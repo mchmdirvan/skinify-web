84c75ecc834da8320dc2b2cc68eeef22b50240d4
Mochamad Irvan <mchmdirvaan@gmail.com>
2025-09-01 14:02:10 +0700

    chore: tidy up

--- app/routes/brand-slug.tsx
+++ app/routes/brand-slug.tsx
@@ -2,7 +2,13 @@ import type { Route } from "./+types/brand-slug";
 import { Fragment } from "react/jsx-runtime";
 import { Link } from "react-router";
 
-import { Card, CardContent, CardHeader, CardTitle } from "~/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card";
 import type { BrandType } from "~/modules/brand/type";
 
 export function meta({ loaderData }: Route.MetaArgs) {
@@ -41,10 +47,10 @@ export default function BrandSlugRoute({ loaderData }: Route.ComponentProps) {
               <Card className="w-full cursor-pointer transition-colors duration-200 hover:border-white xl:min-w-sm">
                 <CardHeader className="flex justify-between">
                   <div className="flex h-20 flex-col justify-between lg:h-48">
-                    <CardTitle className="font-chakra-petch text-xl text-white">
+                    <CardTitle className="font-chakra-petch text-xl">
                       {model.name}
                     </CardTitle>
-                    <p className="text-xs text-white">Premium Skins</p>
+                    <p className="text-xs">Premium Skins</p>
                   </div>
                   <img
                     src={model.imageUrl}
@@ -60,10 +66,10 @@ export default function BrandSlugRoute({ loaderData }: Route.ComponentProps) {
       <section className="my-20 lg:my-40">
         <Card className="">
           <CardContent className="flex min-h-[20vh] flex-col justify-center gap-4">
-            <p className="font-chakra-petch text-center text-xl font-bold text-white md:text-3xl">
+            <CardTitle className="font-chakra-petch text-center text-xl font-bold">
               Can't find your device?
-            </p>
-            <div className="flex justify-center gap-1 text-xs text-nowrap text-white">
+            </CardTitle>
+            <CardDescription className="flex justify-center gap-1 text-xs text-nowrap">
               <a href="tel:+6289755556000" className="hover:underline">
                 +62 897 555 6000
               </a>
@@ -71,7 +77,7 @@ export default function BrandSlugRoute({ loaderData }: Route.ComponentProps) {
               <a href="mailto:skinify@skinify.com" className="hover:underline">
                 skinify@skinify.com
               </a>
-            </div>
+            </CardDescription>
           </CardContent>
         </Card>
       </section>
--- app/routes/shop.tsx
+++ app/routes/shop.tsx
@@ -2,7 +2,13 @@ import { Fragment } from "react/jsx-runtime";
 import type { Route } from "./+types/shop";
 import { Link } from "react-router";
 
-import { Card, CardContent, CardHeader, CardTitle } from "~/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card";
 import type { BrandType } from "~/modules/brand/type";
 
 export function meta({}: Route.MetaArgs) {
@@ -43,12 +49,10 @@ export default function Shop({ loaderData }: Route.ComponentProps) {
                   <Card className="w-full cursor-pointer transition-colors duration-200 hover:border-white xl:min-w-[55vw]">
                     <CardHeader className="flex justify-between">
                       <div className="flex h-20 flex-col justify-between md:h-60 lg:h-96">
-                        <CardTitle className="font-chakra-petch text-white md:text-5xl">
+                        <CardTitle className="font-chakra-petch md:text-5xl">
                           {brand.name}
                         </CardTitle>
-                        <p className="text-xs text-white md:text-base">
-                          Premium Skins
-                        </p>
+                        <p className="text-xs md:text-base">Premium Skins</p>
                       </div>
                       <img
                         src={brand.imageUrl}
@@ -68,10 +72,10 @@ export default function Shop({ loaderData }: Route.ComponentProps) {
                   <Card className="w-full cursor-pointer transition-colors duration-200 hover:border-white">
                     <CardHeader className="flex justify-between">
                       <div className="flex h-20 flex-col justify-between lg:h-48">
-                        <CardTitle className="font-chakra-petch text-xl text-white">
+                        <CardTitle className="font-chakra-petch text-xl">
                           {brand.name}
                         </CardTitle>
-                        <p className="text-xs text-white">Premium Skins</p>
+                        <p className="text-xs">Premium Skins</p>
                       </div>
                       <img
                         src={brand.imageUrl}
@@ -89,10 +93,10 @@ export default function Shop({ loaderData }: Route.ComponentProps) {
       <section className="my-20 lg:my-40">
         <Card className="">
           <CardContent className="flex min-h-[20vh] flex-col justify-center gap-4">
-            <p className="font-chakra-petch text-center text-xl font-bold text-white md:text-3xl">
+            <CardTitle className="font-chakra-petch text-center text-xl font-bold">
               Can't find your device?
-            </p>
-            <div className="flex justify-center gap-1 text-xs text-nowrap text-white">
+            </CardTitle>
+            <CardDescription className="flex justify-center gap-1 text-xs text-nowrap">
               <a href="tel:+6289755556000" className="hover:underline">
                 +62 897 555 6000
               </a>
@@ -100,7 +104,7 @@ export default function Shop({ loaderData }: Route.ComponentProps) {
               <a href="mailto:skinify@skinify.com" className="hover:underline">
                 skinify@skinify.com
               </a>
-            </div>
+            </CardDescription>
           </CardContent>
         </Card>
       </section>
