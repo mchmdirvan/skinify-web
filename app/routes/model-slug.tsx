a8cadc61c7cb01895b78d2975549b67326e66b86
Mochamad Irvan <mchmdirvaan@gmail.com>
2025-09-01 14:46:44 +0700

    style (model-slug) : responsive

--- app/routes/model-slug.tsx
+++ app/routes/model-slug.tsx
@@ -12,6 +12,8 @@ import {
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "~/components/ui/tabs";
 import type { ModelType } from "~/modules/model/type";
 import { Progress } from "~/components/ui/progress";
+import { Fragment } from "react/jsx-runtime";
+import { Separator } from "@radix-ui/react-separator";
 
 export function meta({ loaderData }: Route.MetaArgs) {
   return [
@@ -62,18 +64,18 @@ export default function ModelSlugRoute({ loaderData }: Route.ComponentProps) {
   ];
 
   return (
-    <div className="mx-10 my-10">
-      <section className="relative mt-40 grid grid-cols-2">
-        <div className="sticky top-40 self-start">
-          <img src={products.imageUrl} className="min-w-[600px]" />
+    <Fragment>
+      <section className="relative grid gap-4 lg:mt-40 lg:grid-cols-2">
+        <div className="top-40 self-start lg:sticky">
+          <img src={products.imageUrl} className="xl:min-w-[600px]" />
         </div>
 
         <Card className="border-none bg-transparent">
           <CardHeader>
-            <CardTitle className="font-chakra-petch text-5xl text-white">
+            <CardTitle className="font-chakra-petch text-2xl lg:text-5xl">
               {products.name} Skins
             </CardTitle>
-            <CardDescription className="font-chakra-petch text-white">
+            <CardDescription className="font-chakra-petch">
               Let’s get one thing straight — your {products.name} is cool. But
               not your kind of cool. Wrap your {products.name} with Exacoat’s
               premium skin – no added bulk, just flawless defense. Engineered
@@ -81,7 +83,7 @@ export default function ModelSlugRoute({ loaderData }: Route.ComponentProps) {
             </CardDescription>
           </CardHeader>
           <CardContent>
-            <div className="my-10 grid grid-cols-2 gap-8">
+            <div className="grid gap-4 md:grid-cols-2 lg:my-10 lg:gap-8">
               {products.products.map((product) => (
                 <Link
                   key={product.id}
@@ -89,13 +91,18 @@ export default function ModelSlugRoute({ loaderData }: Route.ComponentProps) {
                 >
                   <Card className="cursor-pointer transition-colors duration-200 hover:border-white">
                     <CardHeader className="flex justify-between">
-                      <div className="flex h-48 flex-col justify-between">
-                        <CardTitle className="font-chakra-petch text-xl text-white">
+                      <div className="flex flex-col justify-between">
+                        <CardTitle className="font-chakra-petch text-xl">
                           {product.name.split("-")[1]}
                         </CardTitle>
-                        <p className="text-white">{product.price}</p>
+                        <p className="text-xs text-nowrap lg:text-base">
+                          Rp. {product.price}
+                        </p>
                       </div>
-                      <img src={product.imageUrl} className="w-[200px]" />
+                      <img
+                        src={product.imageUrl}
+                        className="w-[150px] xl:w-[200px]"
+                      />
                     </CardHeader>
                   </Card>
                 </Link>
@@ -114,7 +121,7 @@ export default function ModelSlugRoute({ loaderData }: Route.ComponentProps) {
             </TabsTrigger>
             <TabsTrigger value="password">FAQs</TabsTrigger>
           </TabsList>
-          <TabsContent value="reviews" className="mt-5 space-y-10">
+          <TabsContent value="reviews" className="mx-5 mt-5 space-y-10">
             <section>
               <p className="text-center text-xs text-amber-400">
                 How we ensure the authenticity of our reviews
@@ -125,15 +132,17 @@ export default function ModelSlugRoute({ loaderData }: Route.ComponentProps) {
               </p>
             </section>
 
-            <Card className="px-14">
-              <CardContent className="flex gap-10">
+            <Card className="lg:px-14">
+              <CardContent className="flex flex-col items-center gap-5 lg:flex-row lg:gap-10">
                 <div className="flex flex-col items-center justify-center">
-                  <p className="text-8xl font-semibold text-amber-400">4.9</p>
+                  <p className="text-5xl font-semibold text-amber-400 lg:text-8xl">
+                    4.9
+                  </p>
                   <p>⭐⭐⭐⭐⭐</p>
                 </div>
-                <div className="min-h-10 self-center border-r border-white p-0">
-                  |
-                </div>
+
+                <div className="min-h-1 min-w-10 self-center border-t border-white p-0 lg:min-h-10 lg:border-t-0 lg:border-r"></div>
+
                 <div>
                   {review.map((rating) => (
                     <div key={rating.stars} className="flex items-center gap-4">
@@ -161,7 +170,9 @@ export default function ModelSlugRoute({ loaderData }: Route.ComponentProps) {
                           </p>
                           <p>⭐⭐⭐⭐⭐</p>
                         </div>
-                        <p className="text-white">{review.date}</p>
+                        <p className="text-xs text-nowrap text-white lg:text-base">
+                          {review.date}
+                        </p>
                       </div>
                       <div className="pt-5 text-xs text-white">
                         {review.content}
@@ -173,24 +184,24 @@ export default function ModelSlugRoute({ loaderData }: Route.ComponentProps) {
             </section>
           </TabsContent>
           <TabsContent value="installation-warranty">
-            <Card className="max-w-2xl px-10 py-14">
+            <Card className="max-w-2xl lg:px-10 lg:py-14">
               <CardHeader>
                 <div className="flex justify-center">
                   <ShieldCheckIcon className="text-amber-400" size={60} />
                 </div>
               </CardHeader>
-              <CardContent className="space-y-5">
-                <p className="text-center text-sm text-white">
+              <CardContent className="space-y-5 text-center text-xs text-white lg:text-sm">
+                <p>
                   We are very confident about how easy the skin installation is
                   for iPhone 16. Don’t trust our word, see the reviews on the
                   previous tab 👈🏻
                 </p>
-                <p className="text-center text-sm text-white">
+                <p>
                   Unlike screen protectors or other skins that can only be
                   applied once, our skin can be removed and applied multiple
                   times if it doesn’t fit.
                 </p>
-                <p className="text-center text-sm text-white">
+                <p>
                   If your skin is damaged on installation within 2 days after
                   the items arrived, we will send you a new one.
                 </p>
@@ -200,7 +211,7 @@ export default function ModelSlugRoute({ loaderData }: Route.ComponentProps) {
         </Tabs>
       </section>
 
-      <section className="mx-60">
+      <section className="md:mx-20 lg:mx-60">
         <p className="font-chakra-petch text-center text-xs text-white">
           You might think that a iPhone 16 case provides enough protection.
           However, the real threat to your iPhone 16 comes from dust, tiny sand
@@ -210,13 +221,13 @@ export default function ModelSlugRoute({ loaderData }: Route.ComponentProps) {
         </p>
       </section>
 
-      <section className="my-40">
+      <section className="my-20 lg:my-40">
         <Card className="">
           <CardContent className="flex min-h-[20vh] flex-col justify-center gap-4">
-            <p className="font-chakra-petch text-center text-3xl font-bold text-white">
+            <CardTitle className="font-chakra-petch text-center text-xl font-bold">
               Have questions about {products.name} Skins?
-            </p>
-            <div className="flex justify-center gap-1 text-white">
+            </CardTitle>
+            <CardDescription className="flex justify-center gap-1 text-xs text-nowrap">
               <a href="tel:+6289755556000" className="hover:underline">
                 +62 897 555 6000
               </a>
@@ -224,10 +235,10 @@ export default function ModelSlugRoute({ loaderData }: Route.ComponentProps) {
               <a href="mailto:skinify@skinify.com" className="hover:underline">
                 skinify@skinify.com
               </a>
-            </div>
+            </CardDescription>
           </CardContent>
         </Card>
       </section>
-    </div>
+    </Fragment>
   );
 }
