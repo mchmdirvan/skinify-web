2428e251575cfbf04765de08572911c45f429256
Mochamad Irvan <mchmdirvaan@gmail.com>
2025-09-01 13:00:52 +0700

    style (shop) : responsive

--- app/routes/shop.tsx
+++ app/routes/shop.tsx
@@ -25,13 +25,13 @@ export default function Shop({ loaderData }: Route.ComponentProps) {
   const brands = loaderData;
 
   return (
-    <div className="mx-10 my-10">
-      <section className="relative mt-40 flex justify-between gap-40">
-        <h2 className="font-chakra-petch sticky top-40 self-start text-7xl font-bold text-white">
+    <div>
+      <section className="relative flex flex-col justify-between gap-10 lg:mt-40 lg:gap-40 xl:flex-row">
+        <h2 className="font-chakra-petch top-40 text-center text-6xl font-bold text-white lg:text-7xl xl:sticky xl:self-start">
           Shop
         </h2>
 
-        <div className="grid grid-cols-2 gap-8">
+        <div className="grid gap-4 md:grid-cols-2 lg:gap-8">
           {brands.map((brand, index) => (
             <Fragment key={brand.id}>
               {index < 4 && (
@@ -40,15 +40,20 @@ export default function Shop({ loaderData }: Route.ComponentProps) {
                   to={`/shop/${brand.slug}`}
                   className="col-span-2"
                 >
-                  <Card className="w-full min-w-[55vw] cursor-pointer transition-colors duration-200 hover:border-white">
+                  <Card className="w-full cursor-pointer transition-colors duration-200 hover:border-white xl:min-w-[55vw]">
                     <CardHeader className="flex justify-between">
-                      <div className="flex h-96 flex-col justify-between">
-                        <CardTitle className="font-chakra-petch text-5xl text-white">
+                      <div className="flex h-20 flex-col justify-between md:h-60 lg:h-96">
+                        <CardTitle className="font-chakra-petch text-white md:text-5xl">
                           {brand.name}
                         </CardTitle>
-                        <p className="text-white">Premium Skins</p>
+                        <p className="text-xs text-white md:text-base">
+                          Premium Skins
+                        </p>
                       </div>
-                      <img src={brand.imageUrl} className="w-[400px]" />
+                      <img
+                        src={brand.imageUrl}
+                        className="w-[100px] md:w-[300px] xl:w-[400px]"
+                      />
                     </CardHeader>
                   </Card>
                 </Link>
@@ -58,17 +63,20 @@ export default function Shop({ loaderData }: Route.ComponentProps) {
                 <Link
                   key={brand.id}
                   to={`/shop/${brand.slug}`}
-                  className="col-span-1"
+                  className="col-span-2 md:col-span-1"
                 >
                   <Card className="w-full cursor-pointer transition-colors duration-200 hover:border-white">
                     <CardHeader className="flex justify-between">
-                      <div className="flex h-48 flex-col justify-between">
+                      <div className="flex h-20 flex-col justify-between lg:h-48">
                         <CardTitle className="font-chakra-petch text-xl text-white">
                           {brand.name}
                         </CardTitle>
-                        <p className="text-white">Premium Skins</p>
+                        <p className="text-xs text-white">Premium Skins</p>
                       </div>
-                      <img src={brand.imageUrl} className="w-[200px]" />
+                      <img
+                        src={brand.imageUrl}
+                        className="w-[100px] lg:w-[200px]"
+                      />
                     </CardHeader>
                   </Card>
                 </Link>
@@ -81,10 +89,10 @@ export default function Shop({ loaderData }: Route.ComponentProps) {
       <section className="my-40">
         <Card className="">
           <CardContent className="flex min-h-[20vh] flex-col justify-center gap-4">
-            <p className="font-chakra-petch text-center text-3xl font-bold text-white">
+            <p className="font-chakra-petch text-center text-xl font-bold text-white md:text-3xl">
               Can't find your device?
             </p>
-            <div className="flex justify-center gap-1 text-white">
+            <div className="flex justify-center gap-1 text-xs text-nowrap text-white">
               <a href="tel:+6289755556000" className="hover:underline">
                 +62 897 555 6000
               </a>
